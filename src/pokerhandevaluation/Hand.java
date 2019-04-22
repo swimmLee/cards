70d71f974f85e7e97a2b015dbfc90350e0f3ea38
Lee Heberer <lheberer@my.wctc.edu>
2019-04-21 23:24:41 -0500

    Initial tests for rank and suit cards complete. Program compiles.

--- src/pokerhandevaluation/Hand.java
+++ src/pokerhandevaluation/Hand.java
@@ -5,51 +5,67 @@ package pokerhandevaluation;
  * @author lhebe
  */
 public class Hand {
-    public int[] rank = new int[5];
+    public int[][] card = new int[5][2];
     DealCard cards = new DealCard();
     
     public Hand(){
     }
 
-    public int[] getRank() {
-        return rank;
+    public int[][] getCard() {
+        return card;
     }
 
-    public void setOneRank(int i, int rank) {
-        this.rank[i] = rank;
+    public void setOneCard(int i, int rank, int suit) {
+        this.card[i][0] = rank;
+        this.card[i][1] = suit;
     }
     
-    public int getOneRank(int i){
-        return rank[i];
+    public int getCardRank(int i){
+        return card[i][0];
+    }
+    
+    public int getCardSuit(int i){
+        return card[i][1];
     }
     
     public void setDealHand(){
-        for(int j=0; j<5; j++){
-            rank[j] = cards.getCard();
+        for(int i=0; i<5; i++){
+            card[i][0] = cards.getRank();
+            card[i][1] = cards.getSuit();
         }
     }
     
     public void setDealHandTest(){
-        rank[0] = cards.getCard0();
-        rank[1] = cards.getCard1();
-        rank[2] = cards.getCard2();
-        rank[3] = cards.getCard3();
-        rank[4] = cards.getCard4();
+        card[0][0] = cards.getRank0();
+        card[1][0] = cards.getRank1();
+        card[2][0] = cards.getRank2();
+        card[3][0] = cards.getRank3();
+        card[4][0] = cards.getRank4();
+        
+        card[0][1] = cards.getSuit0();
+        card[1][1] = cards.getSuit1();
+        card[2][1] = cards.getSuit2();
+        card[3][1] = cards.getSuit3();
+        card[4][1] = cards.getSuit4();
     }
     
     public void setSortHand(){
-        int maxValue, start, maxIndex, pointer;
-        for(start = 0; start<rank.length-1; start++){
-            maxIndex = start;
-            maxValue = rank[start];
-            for(pointer = start+1; pointer < rank.length; pointer++){
-                if(rank[pointer] > maxValue){
-                    maxValue = rank[pointer];
-                    maxIndex = pointer;
+        int maxRankValue, start, maxRankIndex, maxRankSuit, pointer;
+        for(start = 0; start<4; start++){
+            maxRankIndex = start;
+            maxRankValue = card[start][0];
+            maxRankSuit = card[start][1];
+            for(pointer = start+1; pointer < 5; pointer++){
+                if(card[pointer][0] > maxRankValue){
+                    maxRankValue = card[pointer][0];
+                    maxRankSuit = card[pointer][1];
+                    maxRankIndex = pointer;
                 }
             }
-            rank[maxIndex] = rank[start];
-            rank[start] = maxValue;
+            card[maxRankIndex][0] = card[start][0];
+            card[maxRankIndex][1] = card[start][1];
+            card[start][0] = maxRankValue;
+            card[start][1] = maxRankSuit;
         }
     }
     
--- test/TestDevelop/HandJUnitTest.java
+++ test/TestDevelop/HandJUnitTest.java
@@ -11,7 +11,7 @@ import pokerhandevaluation.Hand;
  * @author lhebe
  */
 public class HandJUnitTest {
-    int[] card;
+    int[][] card;
     Hand hand1 = new Hand();
     Hand hand2 = new Hand();
     
@@ -19,45 +19,58 @@ public class HandJUnitTest {
     }
     @Before
     public void setUpCard(){
-        card = new int[5];
-        card[0] = 12;
-        card[1] = 10;
-        card[2] = 11;
-        card[3] = 8;
-        card[4] = 9;
+        card = new int[5][2];
+        card[0][0] = 12;
+        card[1][0] = 10;
+        card[2][0] = 11;
+        card[3][0] = 8;
+        card[4][0] = 9;
+        card[0][1] = 1;
+        card[1][1] = 2;
+        card[2][1] = 3;
+        card[3][1] = 4;
+        card[4][1] = 1;
     }
     
     @Test
     public void setNewCard(){
         for(int i = 0; i<5; i++){
-            hand1.setOneRank(i, card[i]);
-            assertEquals("the values are not equal.",hand1.getOneRank(i),card[i]);
+            hand1.setOneCard(i, card[i][0], card[i][1]);
+            assertEquals("the ranks are not equal.",hand1.getCardRank(i),card[i][0]);
+            assertEquals("the suits are not equal.",hand1.getCardSuit(i),card[i][1]);
         }
-        assertArrayEquals("the values are not equal.",hand1.rank, card);
+        assertArrayEquals("the values are not equal.",hand1.card, card);
     }
     
     @Test
     public void setNewHand2(){
         hand2.setDealHandTest();
         for(int i = 0; i<5; i++){
-            assertEquals("the values are not equal.",hand2.getOneRank(i),card[i]);
+            assertEquals("the ranks are not equal.",hand2.getCardRank(i),card[i][0]);
+            assertEquals("the suits are not equal.",hand2.getCardSuit(i),card[i][1]);
         }
-        assertArrayEquals("the values are not equal.",hand2.rank, card);
+        assertArrayEquals("the values are not equal.",hand2.card, card);
     }
     
     @Test
     public void setNewHand3(){
         hand2.setDealHandTest();
         hand2.setSortHand();
-        card[0] = 12;
-        card[1] = 11;
-        card[2] = 10;
-        card[3] = 9;
-        card[4] = 8;
+        card[0][0] = 12;
+        card[1][0] = 11;
+        card[2][0] = 10;
+        card[3][0] = 9;
+        card[4][0] = 8;
+        card[0][1] = 1;
+        card[1][1] = 3;
+        card[2][1] = 2;
+        card[3][1] = 1;
+        card[4][1] = 4;
         for(int i = 0; i<5; i++){
-            assertEquals("the values are not equal.",hand2.getOneRank(i),card[i]);
+            assertEquals("the ranks are not equal.",hand2.getCardRank(i),card[i][0]);
+            assertEquals("the suits are not equal.",hand2.getCardSuit(i),card[i][1]);
         }
-        assertArrayEquals("the values are not equal.",hand2.rank, card);
+        assertArrayEquals("the Arrays are not equal.",hand2.card, card);
     }
     
     @Ignore
@@ -67,6 +80,6 @@ public class HandJUnitTest {
         for(int i = 0; i<5; i++){
             assertEquals("the values are not equal.",hand2.getOneRank(i),card[i]);
         }
-        assertArrayEquals("the values are not equal.",hand2.rank, card);
+        assertArrayEquals("the values are not equal.",hand2.card, card);
     }
 }
